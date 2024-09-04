82c702f5d990c954389dd0a379e0e340a072d9c8
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-09-04 17:41:57 +0300

    1 крок

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,9 @@
-export const PATH_DB =
+import path from 'node:path';
+
+export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
+
+
+// process.cwd() - повертає абсолютний шлях до проекту
+
+// export const PATH_DB = path.resolve('src', 'db', 'db.json') 
+// це другий спосіб отримати абсолютний шлях до файлу
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,28 @@
-const generateContacts = async (number) => {};
+import * as fs from 'node:fs/promises';
+import DetectFileEncodingAndLanguage from 'detect-file-encoding-and-language';
+
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  // отримуємо інфу про кодування файлу
+  const { encoding } = DetectFileEncodingAndLanguage(PATH_DB);
+
+  // читаем файл таотримуємо старі контакти
+  const oldContacts = fs.readFile(PATH_DB, encoding);
+
+  //створюємо массив, наповнюємо його через метод fill() та мепаем контакти
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+
+  const dataContacts = [...oldContacts, ...newContacts];
+  // записуємо в файл db.json
+  await fs.writeFile(PATH_DB, JSON.stringify(dataContacts, null, 2));
+};
 
 generateContacts(5);
+
+// JSON.stringify другий аргумент - символ замінник, третій аргумент - відступ
+
+// метод toString() перекодовує дані в формат utf-8 за замовчуванням, але можна передати інші формати
+
+// const text = oldContacts.toString()
